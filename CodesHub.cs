d610aebcdfcca1cf90ddb70ddcbdc2520d3d60b5
Maciej Paech <Maciej.Paech@viatechnik.com>
2025-01-19 18:12:39 +0100

    Add project files.

---
+++ CodesHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using static signalr_discount_code_handler.CodesHub;
+
+namespace signalr_discount_code_handler
+{
+    public sealed class CodesHub : Hub<ICodesHub>
+    {
+        public interface ICodesHub
+        {
+            Task RecieveMessage(string message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.RecieveMessage($"New Member ({Context.ConnectionId}): Joined");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.RecieveMessage($"{Context.ConnectionId}: {message}");
+        }
+    }
+}
